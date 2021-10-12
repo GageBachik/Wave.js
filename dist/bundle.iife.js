7c37a4a42675dce40af4010189eb9d7735a8c43b
Gage Bachik <loser@gagebachik.com>
2021-10-12 15:46:18 -0700

    removed retina support for now

--- dist/bundle.iife.js
+++ dist/bundle.iife.js
@@ -1072,21 +1072,9 @@ var Wave = (function () {
 
     if (!canvas) return;
 
-    // Get the device pixel ratio, falling back to 1.
-    let dpr = window.devicePixelRatio || 1;
-    // console.log("dpr", dpr);
-    // Get the size of the canvas in CSS pixels.
-    let rect = canvas.getBoundingClientRect();
-    // Give the canvas pixel dimensions of their CSS
-    // size * the device pixel ratio.
-    canvas.width = rect.width * dpr;
-    canvas.height = rect.height * dpr;
     let ctx = canvas.getContext("2d");
     let h = canvas.height;
     let w = canvas.width;
-    // Scale all drawing operations by the dpr, so you
-    // don't have to worry about the difference.
-    ctx.scale(dpr, dpr);
 
     ctx.strokeStyle = options.colors[0];
     ctx.lineWidth = options.stroke;
